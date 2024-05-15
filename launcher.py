fc86244528dfe75f908b1d1b341df5f4e793bb6a
Flipmixplay <mironhmel@gmail.com>
2024-05-15 20:18:40 +0500

    test

--- launcher.py
+++ launcher.py
@@ -1,43 +1,71 @@
-import os
-import sys
-
-from pathlib import Path
-
-# pip install tqdm
+#импорт библеотек
+from customtkinter import *
+from tkinter import *
 from tqdm import tqdm
+from pathlib import Path
+import os,sys,threading,time,requests,zipfile
 
-# pip install requests
-import requests
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+#создание окна
+tk=CTk()
+tk.title('Лаунчер SWMG')
+tk.iconbitmap(default=resource_path("icon.ico"))
+tk.geometry("900x550+400+300")
+tk.minsize(900,550)
+tk.maxsize(900,550)
+set_appearance_mode("dark")
 
+#функции
 def sizeof_fmt(num: int | float) -> str:
     for x in ['bytes', 'KB', 'MB', 'GB']:
         if num < 1024.0:
             return "%.1f %s" % (num, x)
-
+                
         num /= 1024.0
-
     return "%.1f %s" % (num, 'TB')
 
-
-url = 'https://github.com/Flipmixplay/SWMG/archive/refs/heads/main.zip'
-# Streaming, so we can iterate over the response.
-rs = requests.get(url, stream=True)
-
-# Total size in bytes.
-total_size = int(rs.headers.get('content-length', 0))
-print('From content-length:', sizeof_fmt(total_size))
-
-chunk_size = 1024 * 1024
-num_bars = int(total_size / chunk_size)
-
-file_name = os.path.basename(url)
-
-with open(file_name, mode='wb') as f:
-    for data in tqdm(rs.iter_content(chunk_size), total=num_bars, unit='MB', file=sys.stdout):
-        f.write(data)
+'''def move_progress():
+    steps=10
+    for i in range(steps+1):
+        value = (i * steps) / 100
+        progress_bar.set(value)
+        time.sleep(1)'''
         
-#Проверяем размер файла
+def dowloand():
+    url = 'https://github.com/Flipmixplay/SWMG/blob/main/SWMG.zip'
+    #url = 'https://github.com/Flipmixplay/SWMG/archive/refs/heads/main.zip'
+    # Streaming, so we can iterate over the response.
+    rs = requests.get(url, stream=True)
 
-file_data = open(file_name, mode='rb').read()
-print('File data size:', sizeof_fmt(len(file_data)))        
+    # Total size in bytes.
+    total_size = int(rs.headers.get('content-length', 0))
+    print('Размер файла:', sizeof_fmt(total_size))
+
+    chunk_size = 1024
+    num_bars = int(total_size / chunk_size)
+
+    file_name = os.path.basename(url)
+
+    with open(file_name, mode='wb') as f:
+        for data in tqdm(rs.iter_content(chunk_size), total=num_bars, unit='KB', file=sys.stdout):
+            f.write(data)       
+    #Проверяем размер файла
+    file_data = open(file_name, mode='rb').read()
+    print('Размер скачаного файла', sizeof_fmt(len(file_data)))
+    
+    with zipfile.ZipFile(file_name, 'r') as zip_ref:
+        zip_ref.extractall() 
+    
+#графический интерфейс    
+Update_button=CTkButton(tk,text="Update",font=("MontCinabal", 40),
+                 width=200,height=100,command=dowloand)
+Update_button.place(relx=0.8,rely=0.8,anchor="center")
+
+progress_bar = CTkProgressBar(tk, width=400,progress_color='green')
+progress_bar.place(x=20,y=20)
+
+tk.mainloop()
